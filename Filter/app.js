a22aa3095f662b50ca61b4dc6c124378ec4c1ea2
Aspire212 <1ven970@gmail.com>
2021-06-06 11:34:02 +0300

    add filter no npm

---
+++ Filter/app.js
@@ -0,0 +1,54 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+const list = ['привет', 'пока', 'здравствуй', 'досвидания', 'день', 'ночь', 'полдень'];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: this.props.list,
+      input: '',
+      chbx: false,
+    }
+  }
+  handleCheck = (e) => {
+    let tempList = [...this.props.list]
+    e.target.checked ? this.setState({ list: tempList.sort(), chbx: true }) : this.setState({ list: this.props.list, chbx: false })
+  }
+
+  handleChange = (e) => {
+    this.setState({ list: this.props.list.filter(word => word.includes(e.target.value)), input: e.target.value })
+  }
+  filterList(list, input) {
+    return list.filter(el => el.includes(input.value))
+  }
+
+  clear = () => {
+    if (this.state.input || this.state.chbx) {
+      this.setState({ input: '', chbx: false, list: this.props.list })
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='settings'>
+          <input type="checkbox" name="chbx" onChange={this.handleCheck} checked={this.state.chbx}/>
+          <input type="text" name="input" onChange={this.handleChange} value={this.state.input}/>
+         <input type="button" value="сброс"  onClick={this.clear}/>
+        </div>
+        <ul>
+          {this.state.list.map((word, i) => <li key={i}>{word}</li>)}
+        </ul>
+      </div>
+    )
+  }
+
+}
+
+
+ReactDOM.render(
+  <App list={list} />,
+  document.getElementById('react-app')
+);
